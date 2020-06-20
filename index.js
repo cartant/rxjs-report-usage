ebde189720445977997f02b1579c9469c8b5e580
Nicholas Jamieson <nicholas@cartant.com>
2020-06-20 14:31:40 +1000

    chore: Add globs.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 "use strict";
 
-console.log("find usage and report it");
+const glob = require("glob");
 
-// Information to collect:
-//
-// - rxjs version(s) used
-// - typescript version used
-// - API export usage counts
-//
-// Use Babel/Babylon and its bindings to obtain the counts.
+console.log(`find usage within '${process.cwd()}' and report it`);
+
+Promise.all(
+  [
+    "*.@(js|jsx|ts|tsx)",
+    "!(node_modules)/**/*.@(js|jsx|ts|tsx)",
+    "node_modules/**/@(rxjs|typescript)/package.json",
+  ].map(
+    (pattern) =>
+      new Promise((resolve, reject) =>
+        glob(pattern, {}, (error, files) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(files);
+          }
+        })
+      )
+  )
+).then(([rootFiles, otherFiles, packageFiles]) => {
+  const files = rootFiles.concat(otherFiles);
+  // Information to collect:
+  //
+  // - rxjs version(s) used
+  // - typescript version used
+  // - API export usage counts
+  //
+  // Use Babel/Babylon and its bindings to obtain the counts.
+  console.log(files, packageFiles);
+});
