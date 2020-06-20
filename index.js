949fc6ab22e489893d619edc45457ea0dae8f832
Nicholas Jamieson <nicholas@cartant.com>
2020-06-20 15:16:06 +1000

    chore: Use @babel/parser.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 
+const { parse } = require("@babel/parser");
+const { readFileSync } = require("fs");
 const glob = require("glob");
+const prompts = require("prompts");
 
+const cwd = process.cwd();
+process.chdir("../rxjs-etc");
 console.log(`find usage within '${process.cwd()}' and report it`);
 
 Promise.all(
@@ -11,22 +16,47 @@ Promise.all(
   ].map(
     (pattern) =>
       new Promise((resolve, reject) =>
-        glob(pattern, { ignore: ["node_modules/**/*.{js,jsx,ts,tsx}"] }, (error, files) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(files);
+        glob(
+          pattern,
+          { ignore: ["node_modules/**/*.{js,jsx,ts,tsx}"] },
+          (error, files) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(files);
+            }
           }
-        })
+        )
       )
   )
-).then(([sourceFiles, packageFiles]) => {
-  // Information to collect:
-  //
-  // - rxjs version(s) used
-  // - typescript version used
-  // - API export usage counts
-  //
-  // Use Babel/Babylon and its bindings to obtain the counts.
-  console.log(sourceFiles, packageFiles);
-});
+)
+  .then(([sourceFiles, packageFiles]) => {
+    // Information to collect and report:
+    //
+    // - usage schema version
+    // - rxjs version(s) used
+    // - typescript version used
+    // - API export usage counts
+    //
+    // Use Babel/Babylon and its bindings to obtain the counts.
+    console.log(sourceFiles, packageFiles);
+    sourceFiles.forEach((file) => {
+      if (/\.d\.ts$/.test(file)) {
+        return;
+      }
+      const code = readFileSync(file, "utf8");
+      try {
+        parse(code, {
+          plugins: ["classProperties", "typescript"],
+          sourceType: "unambiguous",
+        });
+      } catch (error) {
+        console.error(`Skipping '${file}' due to parsing error:\n  ${error.message}`);
+      }
+    });
+    packageFiles.forEach((file) => {
+      const content = JSON.parse(readFileSync(file, "utf8"));
+      console.log(content.name, content.version);
+    });
+  })
+  .finally(() => process.chdir(cwd));
