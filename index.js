ddc838cacc0559cb0e388a0cafff5cf4d7dd598d
Nicholas Jamieson <nicholas@cartant.com>
2020-06-20 19:07:55 +1000

    reafactor: Export run directly.

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
 const prompts = require("prompts");
 const collectUsage = require("./collect-usage");
 
-module.exports = {
-  collectUsage,
-  async run(cwd) {
-    console.log(`Collecting usage within '${cwd}' ...`);
-    const usage = await collectUsage(cwd);
-    console.log(JSON.stringify(usage, null, 2));
-  },
+module.exports = async function (cwd) {
+  console.log(`Collecting usage within '${cwd}' ...`);
+  const usage = await collectUsage(cwd);
+  console.log(JSON.stringify(usage, null, 2));
 };
