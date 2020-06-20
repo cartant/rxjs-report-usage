bdee05ad9b22c2b3ad0e69096825a338f00d1181
Nicholas Jamieson <nicholas@cartant.com>
2020-06-20 13:54:22 +1000

    chore: Add editorconfig.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.log("find usage and report it");
+
+// Information to collect:
+//
+// - rxjs version(s) used
+// - typescript version used
+// - API export usage counts
+//
+// Use Babel/Babylon and its bindings to obtain the counts.
