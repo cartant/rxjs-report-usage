e292859c0b088f0be94692f51b458002cec5852d
Nicholas Jamieson <nicholas@cartant.com>
2020-06-20 14:40:13 +1000

    chore: Combine source file globs.

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ console.log(`find usage within '${process.cwd()}' and report it`);
 
 Promise.all(
   [
-    "*.@(js|jsx|ts|tsx)",
-    "!(node_modules)/**/*.@(js|jsx|ts|tsx)",
+    "**/*.{js,jsx,ts,tsx}",
     "node_modules/**/@(rxjs|typescript)/package.json",
   ].map(
     (pattern) =>
       new Promise((resolve, reject) =>
-        glob(pattern, {}, (error, files) => {
+        glob(pattern, { ignore: ["node_modules/**/*.{js,jsx,ts,tsx}"] }, (error, files) => {
           if (error) {
             reject(error);
           } else {
@@ -21,8 +20,7 @@ Promise.all(
         })
       )
   )
-).then(([rootFiles, otherFiles, packageFiles]) => {
-  const files = rootFiles.concat(otherFiles);
+).then(([sourceFiles, packageFiles]) => {
   // Information to collect:
   //
   // - rxjs version(s) used
@@ -30,5 +28,5 @@ Promise.all(
   // - API export usage counts
   //
   // Use Babel/Babylon and its bindings to obtain the counts.
-  console.log(files, packageFiles);
+  console.log(sourceFiles, packageFiles);
 });
